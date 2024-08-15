bb4559623470bfc13d49cc95e837fa198bf0c4e9
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-08-15 23:11:46 +0545

    First Commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+
+
+class X
+{
+
+}
+
+class Y{
+
+}
+public class Test {
+    public static void main(String[] args) {
+        X xObj = new X();
+        // Contains default method from object class.
+//        obj.
+
+        Y yobj = new Y();
+        // Same method from object class
+//        yobj.
+    }
+}

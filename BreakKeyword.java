ae89d582bd2a56dbff621695eb5805b58c303a5e
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-08-25 23:31:21 +0545

    Practicing

---
+++ BreakKeyword.java
@@ -0,0 +1,34 @@
+public class BreakKeyword {
+    public static void main(String[] args) {
+        int i = 0;
+
+//        while(i <= 12){
+//            System.out.println(i);
+//            if(i == 10){
+//                break;
+//            }
+//            i++;
+//        }
+
+        // Do - While Loop
+
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while ( i <= 10);
+
+
+        // Count Down Program
+//        int l = 3;
+//        while( l > 0){
+//            System.out.println(l);
+//            l--;
+//        }
+//        System.out.println("Happy New Year !!!");
+
+
+        String[] names;
+        names = new String[]{"Amir", "Amit"};
+        System.out.println(names[0]);
+    }
+}

8e13ed84735117aa55af1e87aa90802d493d8640
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-08-25 23:36:06 +0545

    Revising

---
+++ TernaryOperator.java
@@ -0,0 +1,50 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+//        int age = 20;
+//        String votingAge = (age >= 18) ? "Allowed": "Not Allowed";
+
+//        int doorCode = 333;
+//        int myDoorCode = 22;
+
+//        if(myDoorCode != doorCode){
+//            System.out.println("Incorrect Door Code");
+//        }else{
+//            System.out.println("Correct");
+//        }
+
+        // Ternary Operator
+
+//        String message = (myDoorCode == doorCode) ? "Correct": "Incorrect";
+//        System.out.println(message);
+
+
+//        int day = 7;
+//        switch(day) {
+//            case 1:
+//                System.out.println("Sunday");
+//            case 2:
+//                System.out.println("Sunday");
+//            case 3:
+//                System.out.println("Sunday");
+//            case 4:
+//                System.out.println("Sunday");
+//            case 5:
+//                System.out.println("Sunday");
+//            case 6:
+//                System.out.println("Sunday");
+//            default:
+//                System.out.println("Sunday");
+
+                String days = "Monday";
+                switch (days) {
+                    case "Sunday":
+                        System.out.println("First day of week");
+                        break;
+                    case "Monday":
+                        System.out.println("Second day of week");
+                        break;
+                    case "Tuesday":
+                            System.out.println("Third Day of week");
+        }
+    }
+}
